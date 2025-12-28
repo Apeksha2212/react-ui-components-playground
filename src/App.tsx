f3c99e29c3da1606be31e208f076b2f89abee73b
vsaitech100 <apeksha.gadekar2212@gmail.com>
2025-12-28 16:40:28 +0530

    custom modal
    custom modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PrimaryButton from './Components/buttons/PrimaryButton';
 import OutlineButton from './Components/buttons/OutlineButton';
 import IconButtonCustom from './Components/buttons/IconButtonCustom';
 import TextInput from './Components/forms/TextInput';
 import SelectInput from './Components/forms/SelectInput';
 import FormExample from './Components/forms/FormExample';
+import { Button, Typography } from '@mui/material';
+import CustomModal from './Components/modal/CustomModal';
 
 function App() {
+    const [open, setOpen] = useState(false);
   return (
     <div className="App">
          Primary Button 
@@ -16,6 +19,24 @@ function App() {
           <TextInput label='click me' value='type here' onChange={()=>alert("cheage")}></TextInput>
          <SelectInput label='click me' value="type here" options={["value", "test", "sample"]} onChange={(e) => alert(e.target.value)}/>
           <FormExample ></FormExample>
+          <Button variant="contained" onClick={() => setOpen(true)}> Open Modal </Button>
+         <CustomModal
+        open={open}
+        onClose={() => setOpen(false)}
+        title="User Details"
+      >
+        <Typography variant="body1">
+          This is a reusable custom modal component.
+        </Typography>
+
+        <Button
+          variant="outlined"
+          sx={{ mt: 2 }}
+          onClick={() => setOpen(false)}
+        >
+          Close
+        </Button>
+      </CustomModal>
 
     </div>
   );
