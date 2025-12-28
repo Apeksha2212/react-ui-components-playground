4dee32f42bb97569750d82df6c89bd56e12cb0e9
vsaitech100 <apeksha.gadekar2212@gmail.com>
2025-12-28 15:33:52 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PrimaryButton from './Components/buttons/PrimaryButton';
+import OutlineButton from './Components/buttons/OutlineButton';
 
 function App() {
   return (
     <div className="App">
          Primary Button 
          <PrimaryButton label="Click Me" onClick={() => alert('Button Clicked!')} />
+         <OutlineButton label="Outline Me" onClick={() => alert('Outline Button Clicked!')} />
     </div>
   );
 }
